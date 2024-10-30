35d01795cfd498c37f325474cbdc2c15f01353c0
MarcosViniSil <marcossilv203@gmail.com>
2024-10-30 20:49:04 -0300

    chore: create connection mysql

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"jumpStart-backEnd/db"
+	"jumpStart-backEnd/repository"
+	
+)
 
 func main() {
-	fmt.Println("Hello World")
+	db := db.GetDB()
+	newsRepository := repository.NewNewsRepository(db)
+	newsRepository.FindAllNews()
+	
 }
+
