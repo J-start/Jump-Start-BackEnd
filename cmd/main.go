3be47b366f9ba2cad7c6de26b0287ec5310229ce
MarcosViniSil <marcossilv203@gmail.com>
2024-11-08 16:57:12 -0300

    fix: ajust date query find shares to run locally

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,16 @@ import (
 	"jumpStart-backEnd/useCase"
 )
 
+
 func main() {
 	db := db.GetDB()
 	newsRepository := repository.NewShareRepository(db)
 	shareUseCase := usecase.NewShareUseCase(newsRepository)
-	
+
 	shareController := controller.NewShareController(shareUseCase)
 
 	http.HandleFunc("/datas/shares", shareController.GetTodaySharesJSON)
 	http.HandleFunc("/datas/shares/offset", shareController.GetSharesSpecifyOffSet)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
-	
 }
-
